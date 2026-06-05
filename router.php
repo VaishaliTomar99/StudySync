37aebf7698684ba4353db8ad45aac48c9db4a678
Vaishali Tomar <tomarvaishali2000@gmail.com>
2026-06-05 16:34:54 +0530

    Updated Railway start command

--- router.php
+++ router.php
@@ -1,13 +1,11 @@
 <?php
 
-if (php_sapi_name() == 'cli-server') {
-    $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
-    $file = __DIR__ . $path;
+$file = __DIR__ . $path;
 
-    if (is_file($file)) {
-        return false;
-    }
+if ($path !== "/" && file_exists($file)) {
+    return false;
 }
 
 require_once __DIR__ . '/index.php';
